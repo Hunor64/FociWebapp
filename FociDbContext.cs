18fe860b30eb574378390baee8542545782aa6a6
hunor64 <nhunor06@gmail.com>
2024-09-13 10:01:55 +0200

    Add project files.

---
+++ FociDbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace FociWebapp
+{
+    public class FociDbContext : DbContext
+    {
+        public FociDbContext(DbContextOptions<FociDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Meccs> Meccsek { get; set; }
+    }
+}
---
+++ Meccs.cs
@@ -0,0 +1,13 @@
+﻿namespace FociWebapp
+{
+    public class Meccs
+    {
+        public int Fordulo { get; set; }
+        public int HazaiEredmeny { get; set; }
+        public int VendegCsapatEredmeny { get; set; }
+        public int HazaiFelidoGol { get; set; }
+        public int VendegFelidoGol { get; set; }
+        public string HazaiCsapatNeve { get; set; }
+        public string VendegCsapatNeve { get; set; }
+    }
+}
